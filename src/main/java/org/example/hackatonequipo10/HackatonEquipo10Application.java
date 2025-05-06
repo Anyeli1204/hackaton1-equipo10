328d3a3f75ec21375688e62517abf035bec4ff0f
Anyeli1204 <anyeli.tamara@utec.edu.pe>
2025-05-06 15:32:04 -0500

    Initial commit

---
+++ src/main/java/org/example/hackatonequipo10/HackatonEquipo10Application.java
@@ -0,0 +1,13 @@
+package org.example.hackatonequipo10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonEquipo10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonEquipo10Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hackatonequipo10/HackatonEquipo10ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hackatonequipo10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonEquipo10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
